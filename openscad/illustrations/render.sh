49d15f227f9610e0c52aae39b2daed1d2045194f
Julian Stirling <julian@julianstirling.co.uk>
2020-12-01 00:11:42 +0000

    Fix mistake in script for image concatenation

--- openscad/illustrations/render.sh
+++ openscad/illustrations/render.sh
@@ -5,4 +5,4 @@ openscad -o "renders/optics_assembly.png" --camera=30,5,60,90,0,110,440 --imgsiz
 for i in {1..5}
   do openscad -o "renders/band$i.png" -D "FRAME=$i;" --camera=-13,13,-30,76,0,216,445 --imgsize=1200,2400 band_insertion_cutaway.scad
 done
-convert band*.png +append band_instruction.png
+convert renders/band*.png +append renders/band_instruction.png
