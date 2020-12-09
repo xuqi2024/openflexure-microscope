214b3e6dd710da8e2bc43b925de989658c7ba7c1
Julian Stirling <julian@julianstirling.co.uk>
2020-12-09 21:23:36 +0000

    Fix render.sh

--- render.sh
+++ render.sh
@@ -13,7 +13,7 @@ convert docs/renders/band*.png +append docs/renders/band_instruction.png
 openscad --hardwarnings -o "docs/renders/brim_and_ties1.png" --camera=-5,22,28,50,0,135,365 --imgsize=2400,2400  rendering/brim_and_ties.scad
 openscad --hardwarnings -o "docs/renders/brim_and_ties2.png" --camera=-4,21,29,181,0,177,450 --imgsize=2400,2400  rendering/brim_and_ties.scad
 
-openscad --hardwarnings -o "docs/renders/actuator_parts.png" -D "FRAME=1;" --camera=2,5,14,33,0,242,360 --imgsize=2400,2000  rendering/actuator_assembly_parts.scad
-openscad --hardwarnings -o "docs/renders/actuator_parts.png" -D "FRAME=2;" --camera=26,22,44,69,0,97,290 --imgsize=2400,2000  rendering/actuator_assembly_nut.scad
-openscad --hardwarnings -o "docs/renders/actuator_parts.png" -D "FRAME=3;" --camera=26,22,44,79,0,173,290 --imgsize=2400,2000  rendering/actuator_assembly_gear.scad
-openscad --hardwarnings -o "docs/renders/actuator_parts.png" -D "FRAME=4;" --camera=26,22,44,79,0,173,290 --imgsize=2400,2000  rendering/actuator_assembly_gear2.scad
+openscad --hardwarnings -o "docs/renders/actuator_assembly_parts.png" -D "FRAME=1;" --camera=2,5,14,33,0,242,360 --imgsize=2400,2000  rendering/actuator_assembly.scad
+openscad --hardwarnings -o "docs/renders/actuator_assembly_nut.png" -D "FRAME=2;" --camera=26,22,44,69,0,97,290 --imgsize=2400,2000  rendering/actuator_assembly.scad
+openscad --hardwarnings -o "docs/renders/actuator_assembly_gear.png" -D "FRAME=3;" --camera=26,22,44,79,0,173,290 --imgsize=2400,2000  rendering/actuator_assembly.scad
+openscad --hardwarnings -o "docs/renders/actuator_assembly_gear2.png" -D "FRAME=4;" --camera=26,22,44,79,0,173,290 --imgsize=2400,2000  rendering/actuator_assembly.scad
