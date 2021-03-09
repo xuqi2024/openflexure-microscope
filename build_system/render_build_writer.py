bd6c3817f936d0d0d099aef86ee17ff00497de4e
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-03-09 18:07:36 +0000

    Add a RenderBuildWriter

---
+++ build_system/render_build_writer.py
@@ -0,0 +1,32 @@
+"""
+In this submodule we create a class that writes a "render.build" file for the microscope renderings.
+"""
+
+import os
+
+from .ninja_writer import NinjaWriter
+
+
+class RenderBuildWriter(NinjaWriter):
+    def __init__(self, build_filename):
+        super().__init__(build_filename=build_filename)
+
+    def __enter__(self, *_):
+        super().__enter__()
+        self._create_rules()
+        return self
+
+    def _create_rules(self):
+        self.rule(
+            "openscad_render",
+            command=f"build_system/openscad_render.py $parameters $in -o $out -d $out.d",
+            depfile="$out.d",
+        )
+
+    def openscad_render(self, output, input_file, parameters=None):
+        self.build(
+            output,
+            rule="openscad_render",
+            inputs=input_file,
+            variables={"parameters": parameters},
+        )
--- render.py
+++ render.py
@@ -1,23 +1,15 @@
 #!/usr/bin/env python3
 import sys
 from ninja import _program
-from build_system.ninja_writer import NinjaWriter
+from build_system.render_build_writer import RenderBuildWriter
 
 NINJA_FILE = "render.ninja"
 
-with NinjaWriter(NINJA_FILE) as w:
-    w.rule(
-        "openscad_render",
-        command=f"build_system/openscad_render.py $parameters $in -o $out -d $out.d",
-        depfile="$out.d",
-    )
-    w.build(
-        "docs/renders/picam1.png"
-        rule="openscad_render",
-        inputs="rendering/prepare_picamera.scad",
-        variables={
-            "parameters": '-D "FRAME=1;" --camera=-6,3,11,46,0,90,140 --imgsize=2400,2000'
-        },
+with RenderBuildWriter(build_filename=NINJA_FILE) as rbw:
+    rbw.openscad_render(
+        "docs/renders/picam1.png",
+        input_file="rendering/prepare_picamera.scad",
+        parameters='-D "FRAME=1;" --camera=-6,3,11,46,0,90,140 --imgsize=2400,2000',
     )
 
 _program("ninja", ["-f", NINJA_FILE] + sys.argv[1:])
