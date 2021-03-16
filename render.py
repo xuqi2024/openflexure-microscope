36e3509b21660e1af3e193a3bcafea86fae206ac
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-03-16 21:36:06 +0000

    Reformat render.py

--- render.py
+++ render.py
@@ -33,29 +33,24 @@ def format_render_params(camera, imgsize, frame=None):
 def generate_optics_assembly_tube_lens(writer):
     input_file = "rendering/rms_optics_assembly.scad"
     camera = Camera(position=[29, 0, 59], angle=[69, 0, 90], distance=290)
+
     for frame in [1, 2, 3]:
         output_file = f"rendering/annotations/optics_assembly_tube_lens{frame}.png"
         parameters = format_render_params(camera, imgsize=[1000, 2000], frame=frame)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
 
 
 def generate_optics_assembly_camera(writer):
     input_file = "rendering/rms_optics_assembly.scad"
     camera = Camera(position=[7, -14, -21], angle=[247, 0, 211], distance=250)
     png_files = []
+
     for frame in [1, 2]:
         output_file = f"docs/renders/optics_assembly_camera{frame}.png"
         png_files.append(output_file)
         parameters = format_render_params(camera, imgsize=[1200, 2000], frame=frame + 3)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
+
     writer.imagemagick_append("docs/renders/optics_assembly_camera.png", png_files)
 
 
@@ -63,15 +58,13 @@ def generate_optics_assembly_objective(writer):
     input_file = "rendering/rms_optics_assembly.scad"
     camera = Camera(position=[2, 2, 25], angle=[55, 0, 90], distance=290)
     png_files = []
+
     for frame in [1, 2]:
         output_file = f"docs/renders/optics_assembly_objective{frame}.png"
         png_files.append(output_file)
         parameters = format_render_params(camera, imgsize=[1200, 2000], frame=frame + 5)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
+
     writer.imagemagick_append("docs/renders/optics_assembly_objective.png", png_files)
 
 
@@ -79,29 +72,24 @@ def generate_optics_assembly_screw(writer):
     input_file = "rendering/rms_optics_assembly.scad"
     camera = Camera(position=[-6.5, 14, 38], angle=[60, 0, 243], distance=290)
     png_files = []
+
     for frame in [1, 2, 3]:
         output_file = f"docs/renders/optics_assembly_screw{frame}.png"
         png_files.append(output_file)
         parameters = format_render_params(camera, imgsize=[1000, 2000], frame=frame + 7)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
+
     writer.imagemagick_append("docs/renders/optics_assembly_screw.png", png_files)
 
 
 def generate_optics_assembly_condenser_lens(writer):
     input_file = "rendering/rms_optics_assembly.scad"
     camera = Camera(position=[29, 0, 59], angle=[69, 0, 90], distance=290)
+
     for frame in [1, 2, 3]:
         output_file = f"rendering/annotations/optics_assembly_condenser_lens{frame}.png"
         parameters = format_render_params(camera, imgsize=[1000, 2000], frame=frame)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
 
 
 def generate_optics_assembled(writer):
@@ -109,26 +97,20 @@ def generate_optics_assembled(writer):
     camera = Camera(position=[30, 5, 60], angle=[90, 0, 110], distance=440)
     output_file = "docs/renders/optics_assembled.png"
     parameters = format_render_params(camera, imgsize=[1200, 2400])
-    writer.openscad_render(
-        output_file,
-        input_file,
-        parameters,
-    )
+    writer.openscad_render(output_file, input_file, parameters)
 
 
 def generate_band(writer):
     input_file = "rendering/band_insertion_cutaway.scad"
     camera = Camera(position=[-13, 13, -30], angle=[76, 0, 216], distance=445)
     png_files = []
+
     for frame in [1, 2, 3, 4, 5]:
         output_file = f"docs/renders/band{frame}.png"
         png_files.append(output_file)
         parameters = format_render_params(camera, imgsize=[1200, 2400], frame=frame)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
+
     writer.imagemagick_append("docs/renders/band_instruction.png", png_files)
 
 
@@ -170,11 +152,7 @@ def generate_actuator_assembly(writer):
     for i, camera in enumerate(cameras):
         output_file = os.path.join("docs/renders/", pngs[i])
         parameters = format_render_params(camera, imgsize=[2400, 2000], frame=i + 1)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
 
 
 def generate_picam(writer):
@@ -183,11 +161,7 @@ def generate_picam(writer):
     for frame in [1, 2, 3]:
         output_file = f"docs/renders/picam{frame}.png"
         parameters = format_render_params(camera, imgsize=[2400, 2000], frame=frame)
-        writer.openscad_render(
-            output_file,
-            input_file,
-            parameters,
-        )
+        writer.openscad_render(output_file, input_file, parameters)
 
 
 with RenderBuildWriter(build_filename=NINJA_FILE) as rbw:
