4633f19980744166ab7ad15499bc27573bd628b4
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2021-03-16 19:42:55 +0000

    Port rest of openscad renders to render.py

--- render.py
+++ render.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import os
 import subprocess
 from dataclasses import dataclass
 from ninja import _program
@@ -30,8 +31,8 @@ def format_render_params(camera, imgsize, frame=None):
 
 
 def generate_optics_assembly_tube_lens(writer):
-    camera = Camera(position=[29, 0, 59], angle=[69, 0, 90], distance=290)
     input_file = "rendering/rms_optics_assembly.scad"
+    camera = Camera(position=[29, 0, 59], angle=[69, 0, 90], distance=290)
     for frame in [1, 2, 3]:
         output_file = f"rendering/annotations/optics_assembly_tube_lens{frame}.png"
         parameters = format_render_params(camera, imgsize=[1000, 2000], frame=frame)
@@ -43,8 +44,8 @@ def generate_optics_assembly_tube_lens(writer):
 
 
 def generate_optics_assembly_camera(writer):
-    camera = Camera(position=[7, -14, -21], angle=[247, 0, 211], distance=250)
     input_file = "rendering/rms_optics_assembly.scad"
+    camera = Camera(position=[7, -14, -21], angle=[247, 0, 211], distance=250)
     for frame in [1, 2]:
         output_file = f"docs/renders/optics_assembly_camera{frame}.png"
         parameters = format_render_params(camera, imgsize=[1200, 2000], frame=frame + 3)
@@ -56,8 +57,8 @@ def generate_optics_assembly_camera(writer):
 
 
 def generate_optics_assembly_objective(writer):
-    camera = Camera(position=[2, 2, 25], angle=[55, 0, 90], distance=290)
     input_file = "rendering/rms_optics_assembly.scad"
+    camera = Camera(position=[2, 2, 25], angle=[55, 0, 90], distance=290)
     for frame in [1, 2]:
         output_file = f"docs/renders/optics_assembly_objective{frame}.png"
         parameters = format_render_params(camera, imgsize=[1200, 2000], frame=frame + 5)
@@ -69,11 +70,94 @@ def generate_optics_assembly_objective(writer):
 
 
 def generate_optics_assembly_screw(writer):
-    camera = Camera(position=[-6.5, 14, 38], angle=[60, 0, 243], distance=290)
     input_file = "rendering/rms_optics_assembly.scad"
+    camera = Camera(position=[-6.5, 14, 38], angle=[60, 0, 243], distance=290)
     for frame in [1, 2, 3]:
         output_file = f"docs/renders/optics_assembly_screw{frame}.png"
         parameters = format_render_params(camera, imgsize=[1000, 2000], frame=frame + 7)
+        writer.openscad_render(
+            output_file,
+            input_file,
+            parameters,
+        )
+
+
+def generate_optics_assembly_condenser_lens(writer):
+    input_file = "rendering/rms_optics_assembly.scad"
+    camera = Camera(position=[29, 0, 59], angle=[69, 0, 90], distance=290)
+    for frame in [1, 2, 3]:
+        output_file = f"rendering/annotations/optics_assembly_condenser_lens{frame}.png"
+        parameters = format_render_params(camera, imgsize=[1000, 2000], frame=frame)
+        writer.openscad_render(
+            output_file,
+            input_file,
+            parameters,
+        )
+
+
+def generate_optics_assembled(writer):
+    input_file = "rendering/optics_assembly.scad"
+    camera = Camera(position=[30, 5, 60], angle=[90, 0, 110], distance=440)
+    output_file = "docs/renders/optics_assembled.png"
+    parameters = format_render_params(camera, imgsize=[1200, 2400])
+    writer.openscad_render(
+        output_file,
+        input_file,
+        parameters,
+    )
+
+
+def generate_band(writer):
+    input_file = "rendering/band_insertion_cutaway.scad"
+    camera = Camera(position=[-13, 13, -30], angle=[76, 0, 216], distance=445)
+    for frame in [1, 2, 3, 4, 5]:
+        output_file = f"docs/renders/band{frame}.png"
+        parameters = format_render_params(camera, imgsize=[1200, 2400], frame=frame)
+        writer.openscad_render(
+            output_file,
+            input_file,
+            parameters,
+        )
+
+
+def generate_brim_and_ties(writer):
+    input_file = "rendering/brim_and_ties.scad"
+    cameras = [
+        Camera(position=[5, 22, 28], angle=[50, 0, 135], distance=365),
+        Camera(position=[-4, 21, 29], angle=[206, 0, 177], distance=450),
+    ]
+    for i, camera in enumerate(cameras):
+        frame = i + 1
+        output_file = f"docs/renders/brim_and_ties{frame}.png"
+        parameters = format_render_params(camera, imgsize=[2400, 2400], frame=frame)
+        writer.openscad_render(
+            output_file,
+            input_file,
+            parameters,
+        )
+
+
+def generate_actuator_assembly(writer):
+    input_file = "rendering/actuator_assembly.scad"
+    cameras = [
+        Camera(position=[2, 5, 14], angle=[33, 0, 242], distance=360),
+        Camera(position=[4, 35, 35], angle=[71, 0, 186], distance=330),
+        Camera(position=[4, 35, 35], angle=[71, 0, 186], distance=330),
+        Camera(position=[4, 35, 35], angle=[71, 0, 186], distance=330),
+        Camera(position=[4, 35, 35], angle=[71, 0, 186], distance=330),
+        Camera(position=[4, 35, 35], angle=[71, 0, 186], distance=330),
+    ]
+    pngs = [
+        "actuator_assembly_parts.png",
+        "actuator_assembly_nut.png",
+        "actuator_assembly_gear.png",
+        "actuator_assembly_gear2.png",
+        "actuator_assembly_x.png",
+        "actuators_assembled.png",
+    ]
+    for i, camera in enumerate(cameras):
+        output_file = os.path.join("docs/renders/", pngs[i])
+        parameters = format_render_params(camera, imgsize=[2400, 2000], frame=i + 1)
         writer.openscad_render(
             output_file,
             input_file,
@@ -99,6 +183,11 @@ with RenderBuildWriter(build_filename=NINJA_FILE) as rbw:
     generate_optics_assembly_camera(rbw)
     generate_optics_assembly_objective(rbw)
     generate_optics_assembly_screw(rbw)
+    generate_optics_assembly_condenser_lens(rbw)
+    generate_optics_assembled(rbw)
+    generate_band(rbw)
+    generate_brim_and_ties(rbw)
+    generate_actuator_assembly(rbw)
     generate_picam(rbw)
 
 subprocess.run(
--- render.sh
+++ render.sh
@@ -12,26 +12,7 @@ convert docs/renders/optics_assembly_objective*.png +append docs/renders/optics_
 
 convert docs/renders/optics_assembly_screw*.png +append docs/renders/optics_assembly_screw.png
 
-build_system/openscad_render.py -o "rendering/annotations/optics_assembly_condenser_lens1.png" -D "FRAME=1;" --camera=29,0,59,69,0,90,290 --imgsize=1000,2000  rendering/optics_assembly.scad
-build_system/openscad_render.py -o "rendering/annotations/optics_assembly_condenser_lens2.png" -D "FRAME=2;" --camera=29,0,59,69,0,90,290 --imgsize=1000,2000  rendering/optics_assembly.scad
-build_system/openscad_render.py -o "rendering/annotations/optics_assembly_condenser_lens3.png" -D "FRAME=3;" --camera=29,0,59,69,0,90,290 --imgsize=1000,2000  rendering/optics_assembly.scad
-
-build_system/openscad_render.py -o "docs/renders/optics_assembled.png" -D "FRAME=4;" --camera=30,5,60,90,0,110,440 --imgsize=1200,2400  rendering/optics_assembly.scad
-
-for i in {1..5}
-  do build_system/openscad_render.py -o "docs/renders/band$i.png" -D "FRAME=$i;" --camera=-13,13,-30,76,0,216,445 --imgsize=1200,2400 rendering/band_insertion_cutaway.scad
-done
 convert docs/renders/band*.png +append docs/renders/band_instruction.png
-
-build_system/openscad_render.py -o "docs/renders/brim_and_ties1.png" --camera=-5,22,28,50,0,135,365 --imgsize=2400,2400  rendering/brim_and_ties.scad
-build_system/openscad_render.py -o "docs/renders/brim_and_ties2.png" --camera=-4,21,29,206,0,177,450 --imgsize=2400,2400  rendering/brim_and_ties.scad
-
-build_system/openscad_render.py -o "docs/renders/actuator_assembly_parts.png" -D "FRAME=1;" --camera=2,5,14,33,0,242,360 --imgsize=2400,2000  rendering/actuator_assembly.scad
-build_system/openscad_render.py -o "docs/renders/actuator_assembly_nut.png" -D "FRAME=2;" --camera=4,35,35,71,0,186,330 --imgsize=2400,2000  rendering/actuator_assembly.scad
-build_system/openscad_render.py -o "docs/renders/actuator_assembly_gear.png" -D "FRAME=3;" --camera=4,35,35,71,0,186,330 --imgsize=2400,2000  rendering/actuator_assembly.scad
-build_system/openscad_render.py -o "docs/renders/actuator_assembly_gear2.png" -D "FRAME=4;" --camera=4,35,35,71,0,186,330 --imgsize=2400,2000  rendering/actuator_assembly.scad
-build_system/openscad_render.py -o "docs/renders/actuator_assembly_x.png" -D "FRAME=5;" --camera=4,35,35,71,0,186,330 --imgsize=2400,2000  rendering/actuator_assembly.scad
-build_system/openscad_render.py -o "docs/renders/actuators_assembled.png" -D "FRAME=6;" --camera=4,35,35,71,0,186,330 --imgsize=2400,2000  rendering/actuator_assembly.scad
 
 
 # inkscape annotations, make sure SVG uses relative links.
