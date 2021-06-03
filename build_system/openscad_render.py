3ee5a0a0faec518eb0ede159d9993df16e9056da
Julian Stirling <julian@julianstirling.co.uk>
2021-06-03 21:15:09 +0100

    Stop using util.get_openscad_exe in openscad_render as it is a standalone script

--- build_system/openscad_render.py
+++ build_system/openscad_render.py
@@ -10,15 +10,19 @@ instead a blank .png is output. See: https://github.com/openscad/openscad/issues
 import subprocess
 import sys
 import re
-#Note that because of how the script is called externally the way we import confuses pylint
-from util import get_openscad_exe #pylint: disable=no-name-in-module
+
 
 def main(args):
     """
     Run openscad with hardwarnings and return any messages from std_error on std_out.
     If OpenSCAD warnings are thrown exit the program with an error.
     """
-    executable = get_openscad_exe()
+
+    #Note not using util.get_openscad_exe to keep this a stand alone script
+    if sys.platform.startswith("darwin"):
+        executable = "/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD"
+    else:
+        executable = "openscad"
     ret = subprocess.run([executable, '--hardwarnings'] + args, check=True, capture_output=True)
     std_err = ret.stderr.decode('UTF-8')
     print(std_err)
