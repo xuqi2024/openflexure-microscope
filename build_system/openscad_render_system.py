c95f64dcb743d6cdccdc104d19d0bd317b3455ae
Julian Stirling <julian@julianstirling.co.uk>
2021-10-26 16:01:07 +0100

    Clean up render system code and improve comments

--- build_system/openscad_render_system.py
+++ build_system/openscad_render_system.py
@@ -1,10 +1,25 @@
 """
-A simple but fast render system for openscad which generated .scad files on the fly and uses
+A simple fast-render system for openscad which generated .scad files on the fly and uses
 the openscad animation feature to speed up rendering similar frames.
+
+The render system struggles somewhat in Docker due to what appears to be a shared memory
+issue. A summary of the rendering strategy and the docker issue is given below:
+
+* OpenSCAD rendering uses OpenGL to create the final render, much of the time consuming
+  geometry information is cached when OpenSCAD is open
+* When OpenSCAD closes the cached information is lost
+* Creating a series of images in OpenSCAD as an animation speeds things up as it can
+  used cached geometries for future renders.
+* This works fine in testing, but  can fail in Docker during long animations. It seems
+  that OpenGL tries to access shared memory in a way that is forbidden by docker, this
+  causes OpenSCAD to crash. The underlying cause and how to fix it has eluded us
+* However, OpenSCAD exports each frame to a PNG as it is running. This means that if it
+  crashes during frame 6 of an 11 render animation the first 5 renders are exported
+  correctly.
+* This render system detects these failures and re-run the remaining frames.
 """
 
 import subprocess
-import sys
 import shutil
 import re
 import os
@@ -40,9 +55,12 @@ class ScadRender():
         self.scad = scad
         self.imgsize = tuple(imgsize)
         self.camera = camera
-        self.rendered = False
 
 def out_file(hash_name, i):
+    """
+    Return the output filename givenn the hash of the render job and the frame
+    number
+    """
     return os.path.join(gettempdir(), f'frame{hash_name}-{i:05}.png')
 
 
@@ -67,13 +85,14 @@ class RenderSystem():
 
     def register_scad_render(self, render):
         """
-        Register an openscad render (ScadRender object) to be run when when RenderSystem.render() is run
+        Register an openscad render (ScadRender object) to be run when when
+        RenderSystem.render() is run.
         """
         self._renders.append(render)
 
     def register_imagemagick_sequence(self, outfile, input_files):
         """
-        Register an image sequence (and output file and a list of input files).
+        Register an image sequence (an output file and a list of input files).
         Each image in the sequence will be appended left to right with imagemagick once
         all openscad renders have been performed
         """
@@ -81,9 +100,9 @@ class RenderSystem():
 
     def register_inkscape_annotation(self, outfile, svg_file):
         """
-        Register an image sequence (and output file and a list of input files).
-        Each image in the sequence will be appended left to right with imagemagick once
-        all openscad renders have been performed
+        Register an inkscape annotation (an output file and an input SVG file).
+        The SVG files should have linked PNG files. Once OpenSCAD has produced
+        all PNG files, inkscape will be run to add annotations.
         """
         self._inkscape_annotations.append((outfile, svg_file))
 
@@ -116,6 +135,9 @@ class RenderSystem():
 
 
     def _run_openscad(self):
+        """
+        Run openscad for all renders
+        """
         tmpdir = gettempdir()
         tmpscad = os.path.join(tmpdir, 'scadfile.scad')
         #note that openscad will append 00000, 00001, etc to the name just before the extension
@@ -123,61 +145,75 @@ class RenderSystem():
         for size in sizes:
             renders = [render for render in self._renders if render.imgsize==size]
             while len(renders) > 0:
-                hash_name = str(uuid.uuid4())
-                output_template = os.path.join(tmpdir, f'frame{hash_name}-.png')
-                n_renders = len(renders)
                 scad = _create_scad_for_renders(renders)
                 with open(tmpscad, 'w') as scadfile:
                     scadfile.write(scad)
-                executable = get_openscad_exe()
 
-                imgsize_str = ",".join([str(i) for i in size])
-                imgsize_arg = f'--imgsize={imgsize_str}'
-                #note we cannot use hardwarnings as we change the camera angle which always throws
-                # a stupid warning see:
-                # https://github.com/openscad/openscad/issues/3646
-                # https://github.com/openscad/openscad/pull/3660/
-                scad_args = ['--animate', str(n_renders), imgsize_arg, '-o', output_template]
-                print(f"\nStarting OpenSCAD for images of size {imgsize_str}...\n\n")
-                try:
-                    ret = subprocess.run(
-                        [executable, tmpscad] + scad_args,
-                        check=True,
-                        capture_output=True
-                    )
-                    std_err = ret.stderr.decode('UTF-8')
-                    #All images are now rendered
-                    for render in renders:
-                        render.rendered = True
-                except subprocess.CalledProcessError as error:
-                    #If there is an error not all images were rendered
-                    std_err = error.stderr.decode('UTF-8')
-                    if "X Error of failed request" in std_err:
-                        print("\n\nPartial fail due to Docker OpenGL issue. "
-                              "Missing renders will be reggenerated\n\n")
-                    else:
-                        print(std_err)
-                        raise
+                rerender = run_openscad_animation(tmpscad, renders, size)
 
-                print(std_err)
-                warns = re.findall(r'^WARNING:.*?%', std_err, flags=re.MULTILINE)
-
-                if warns != []:
-                    if warns[0] != r'WARNING: Viewall and autocenter disabled in favor of $vp*':
-                        sys.exit(1)
-
-                rerender = _copy_renders(renders, hash_name)
-                renders = rerender
-                if len(renders)==n_renders:
+                if len(rerender)==len(renders):
                     RuntimeError("No renders produced for this job. Renders failed!")
-                if len(renders)>0:
+                if len(rerender)>0:
                     # Empty lines are not returned in gitlab CI.
                     # Using starts to make this line obvious
-                    print(f"\n*\n*\nRe-rendering {len(renders)} of {n_renders}\n*\n*\n")
+                    print(f"\n*\n*\nRe-rendering {len(rerender)} of {len(renders)}\n*\n*\n")
+                renders = rerender
 
-def _copy_renders(renders, hash_name):
+def run_openscad_animation(filename, renders, size):
+    """
+    Run an openscad file which produces an animation. Renders gives a list of the desired name
+    for each frame in order.
+    """
+    executable = get_openscad_exe()
+    hash_name = str(uuid.uuid4())
+    output_template = os.path.join(gettempdir(), f'frame{hash_name}-.png')
+    n_renders = len(renders)
+    imgsize_str = ",".join([str(i) for i in size])
+    imgsize_arg = f'--imgsize={imgsize_str}'
+    #Hardwarnings not in use. See check_openscad_warnings() for more details
+    scad_args = ['--animate', str(n_renders), imgsize_arg, '-o', output_template]
+    print(f"\nStarting OpenSCAD for images of size {imgsize_str}...\n\n")
+    try:
+        ret = subprocess.run(
+            [executable, filename] + scad_args,
+            check=True,
+            capture_output=True
+        )
+        std_err = ret.stderr.decode('UTF-8')
+        print(std_err)
+    except subprocess.CalledProcessError as error:
+        #If there is an error not all images were rendered
+        std_err = error.stderr.decode('UTF-8')
+        if "X Error of failed request" in std_err:
+            print(std_err)
+            print("\n\nPartial fail due to Docker OpenGL issue. "
+                    "Missing renders will be regenerated\n\n")
+        else:
+            print(std_err)
+            raise
+
+    check_openscad_warnings(std_err)
+    return copy_renders(renders, hash_name)
+
+def check_openscad_warnings(std_err):
+    """
+    Check openscad warnings and throw error if unexpected warning is thrown.
+
+    We cannot use hardwarnings as we change the camera angle during renders. This always
+    throws an annoyong warning. See:
+    # https://github.com/openscad/openscad/issues/3646
+    # https://github.com/openscad/openscad/pull/3660/
+    """
+    warns = re.findall(r'^WARNING:.*?%', std_err, flags=re.MULTILINE)
+
+    if warns != []:
+        if warns[0] != r'WARNING: Viewall and autocenter disabled in favor of $vp*':
+            RuntimeError("Error. OpenSCAD code generates unexpected warnings")
+
+def copy_renders(renders, hash_name):
     """
     Copy the output files from the temp directory to their desired location.
+    Return a list of any missing renders so that they can be rerendered
     """
     rerender = []
     for i, render in enumerate(renders):
