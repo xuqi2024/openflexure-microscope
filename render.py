fd4bc6d48101c52b84d7083787d313a044b892a3
williamwadsworth <w.j.wadsworth@bath.ac.uk>
2024-12-08 11:53:00 +0000

    register prepare manual body in render.py

--- render.py
+++ render.py
@@ -316,11 +316,13 @@ def register_prepare_stand(rendersystem):
         Camera(position=[-9, 20, 37.5], angle=[34, 0, 235], distance=450),
     ]
     imgsize = [2400, 2000]
-    for i, camera in enumerate(cameras):
-        output_file = f"docs/renders/prepare_stand{i+1}.png"
-        scad = f"render_prepare_stand({i+1});"
-        render = ScadRender(output_file, input_file, scad, imgsize, camera)
-        rendersystem.register_scad_render(render)
+    for body in ["", "_manual"]:
+        manual = str(body == "_manual").lower()
+        for i, camera in enumerate(cameras):
+            output_file = f"docs/renders/prepare_stand{body}{i+1}.png"
+            scad = f"render_prepare_stand({i+1},{manual});"
+            render = ScadRender(output_file, input_file, scad, imgsize, camera)
+            rendersystem.register_scad_render(render)
 
 def register_actuator_assembly(rendersystem):
     input_file = "rendering/actuator_assembly.scad"
