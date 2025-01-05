0ad76bc1996502c9c79d4bd81a2422e206ed6768
williamwadsworth <w.j.wadsworth@bath.ac.uk>
2025-01-05 16:09:33 +0000

    pre-build stl for separate z actuator render

--- render.py
+++ render.py
@@ -524,6 +524,11 @@ def register_rendered_microscope_stl(rendersystem, force_clean):
     parameters = {"VERSION_STRING": version_str}
     rendersystem.register_render_stl(input_file, parameters)
 
+def register_rendered_separate_z_actuator_stl(rendersystem, force_clean):
+    input_file = "rendering/librender/rendered_separate_z_actuator.scad"
+    parameters = {}
+    rendersystem.register_render_stl(input_file, parameters)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -539,6 +544,7 @@ def main():
     rendersystem = RenderSystem()
     rendersystem.register_zip_assets('rendering/librender/hardware.zip')
     register_rendered_microscope_stl(rendersystem, force_clean=args.force_clean)
+    register_rendered_separate_z_actuator_stl(rendersystem, force_clean=args.force_clean)
     #Register all openscad renders (and associated post processing)
     register_rms_optics_assembly(rendersystem)
     register_low_cost_optics_assembly(rendersystem)
