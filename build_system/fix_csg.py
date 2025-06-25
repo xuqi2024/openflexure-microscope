5907868ff90aaf8f092b02f79752d7fde4c9b040
Florian Festi <florian@festi.info>
2025-06-25 23:57:50 +0200

    Adjust paths of import statements
    import statements of the .csg files break as they are no longer in the
    location of the .scad files. Read the original locations from the dependency
    files and replace the path.
    
    This worked so far because the build.py copies the imported logo dxf
    files to docs/models/libs. This might no longer be needed. Using the files
    from the openscad/ subdir also revents the stl to be rebuild every time.
    
    Code is a bit complicated as it does not rely on there being only one file
    with the same basename being imported. In case there are multiple we use the
    one with the longer matching path.
    
    This change is needed for the Delta to use the build_system.

--- build_system/fix_csg.py
+++ build_system/fix_csg.py
@@ -7,6 +7,30 @@ building in CI/git-managed files means timestamps are unreliable.
 
 import argparse
 import re
+import os.path
+
+def get_dependency(deps, fn):
+    bn = os.path.basename(fn)
+    if not bn in deps:
+        raise ValueError()
+    fn_dirs = fn.split("/")
+    # search for longest match
+    match_len = 0
+    match_fn = fn
+    for dep in deps[bn]:
+        dep_dirs = dep.split("/")
+        for n, (f, d) in enumerate(zip(reversed(fn_dirs), reversed(dep_dirs))):
+            if f != d:
+                if n > match_len:
+                    match_len = n
+                    match_fn = dep
+                break
+        else:
+            # All matched
+            match_len = n + 1
+            match_fn = dep
+
+    return match_fn
 
 def fix_csg(input_fname, output_fname):
     """Fix a CSG file so it will compile in OpenSCAD without warnings.
@@ -15,11 +39,21 @@ def fix_csg(input_fname, output_fname):
     * Strip `timestamp` arguments (which occur in `import` module calls)
     * Create a `.d` dependency file for the `.fixed.csg` for `ninja` to use. This file the depends only on the `.csg` file.
     """
+
+    dependencies = {}
+    with open(input_fname + '.d', "r", encoding='utf-8') as infile:
+        for line in infile:
+            fn = line.strip().split()[0]
+            dependencies.setdefault(os.path.basename(fn), []).append(fn)
+
     with open(input_fname, "r", encoding='utf-8') as infile, open(output_fname, "w", encoding='utf-8') as outfile:
         for line in infile:
-            outfile.write(
-                re.sub(r", timestamp = [\d]+", "", line)
-            )
+            m = re.search(r'import\(file = "([^"]+)"', line)
+            if m:
+                line = line.replace(m.group(1), get_dependency(dependencies, m.group(1)))
+                line = re.sub(r", timestamp = [\d]+", "", line)
+
+            outfile.write(line)
 
     with open(output_fname + ".d", "w", encoding='utf-8') as outfile:
         outfile.write(f"{output_fname}: \\\n")
