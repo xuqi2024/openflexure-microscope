07e8832ee54419ee50bf7b42ebbf1dd53e3aa61a
Florian Festi <florian@festi.info>
2025-06-25 23:57:50 +0200

    Adjust paths of import statements
    import statements of the .csg files break as they are no longer in the
    location of the .scad files. Read the original locations from the dependency
    files and replace the path.
    
    This worked so far because the build.py copies the imported logo dxf
    files to docs/models/libs. This might no longer be needed. Using the files
    from the openscad/ subdir also revents the stl to be rebuild every time.
    
    Code is a bit complicated as it does not rely on there being only one file
    with the same basename being imported. In case there are multiple we use the
    one with the longer matching path.
    
    This change is needed for the Delta to use the build_system.

--- build_system/fix_csg.py
+++ build_system/fix_csg.py
@@ -1,25 +1,72 @@
 """Fix CSG files so they are valid, time-independent SCAD code
 
-At the moment, we just strip out timestamps from import statements, because
-building in CI/git-managed files means timestamps are unreliable.
+Strip out timestamps from import statements, because building
+in CI/git-managed files means timestamps are unreliable.
+
+Replace filenames of imports with the ones from the dependency file
+as they are absolute and work even if the csg file is elsewhere.
+
+Place a dependency fill beside the new file to allow Ninja figuring things out.
 """
 
 
 import argparse
 import re
+import os.path
+
+def get_dependency(deps, fname):
+    """Find dependency best matching fname.
+    Basenames and as many surrounding directories as possible should match.
+    This allows imports of different files with the same basename without
+    mixups.
+    """
+    fname_dirs = fname.split("/")
+    # search for longest match
+    match_len = 0
+    match_fname = fname
+    for dep in deps[os.path.basename(fname)]:
+        dep_dirs = dep.split("/")
+        # Compare sub dirs from bottom up
+        for n, (file_dir, dep_dir) in enumerate(
+                zip(reversed(fname_dirs), reversed(dep_dirs))):
+            if file_dir != dep_dir:
+                if n > match_len:
+                    match_len = n
+                    match_fname = dep
+                break
+        else:
+            # All matched
+            match_len = n + 1
+            match_fname = dep
+
+    return match_fname
 
 def fix_csg(input_fname, output_fname):
     """Fix a CSG file so it will compile in OpenSCAD without warnings.
 
     This performs only two operations:
     * Strip `timestamp` arguments (which occur in `import` module calls)
-    * Create a `.d` dependency file for the `.fixed.csg` for `ninja` to use. This file the depends only on the `.csg` file.
+    * Create a `.d` dependency file for the `.fixed.csg` for `ninja` to use.
+      This file the depends only on the `.csg` file.
+    * Replace filenames in import statements with the absolute ones from the
+      dependency file of the original.
     """
-    with open(input_fname, "r", encoding='utf-8') as infile, open(output_fname, "w", encoding='utf-8') as outfile:
+
+    dependencies = {}
+    with open(input_fname + '.d', "r", encoding='utf-8') as infile:
         for line in infile:
-            outfile.write(
-                re.sub(r", timestamp = [\d]+", "", line)
-            )
+            fn = line.strip().split()[0]
+            dependencies.setdefault(os.path.basename(fn), []).append(fn)
+
+    with (open(input_fname, "r", encoding='utf-8') as infile,
+          open(output_fname, "w", encoding='utf-8') as outfile):
+        for line in infile:
+            m = re.search(r'import\(file = "([^"]+)"', line)
+            if m:
+                line = line.replace(m.group(1), get_dependency(dependencies, m.group(1)))
+                line = re.sub(r", timestamp = [\d]+", "", line)
+
+            outfile.write(line)
 
     with open(output_fname + ".d", "w", encoding='utf-8') as outfile:
         outfile.write(f"{output_fname}: \\\n")
